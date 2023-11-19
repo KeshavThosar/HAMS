c030c374a9ef6993b8ede8056b6d1a44fa4f074b
Keshav Thosar <keshavjaythosar@gmail.com>
2023-11-20 00:43:50 +0530

    Created Data and User Models for testapp Added test users for supplier and hospital

---
+++ mainsite/testapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register([ Organization,
+                      OrganizationUser,
+                      OrganizationStock,
+                      Asset,
+                      PurchaseOrder,
+                      PurchaseOrderDetails,
+                      ])
+
